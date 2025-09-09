9a9410f1c461b37a64927931ee386f93771f2ac4
Mohamed Ashraf <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2025-09-09 23:47:36 +0300

    clean code (#21)

--- src/core/graph.cpp
+++ src/core/graph.cpp
@@ -22,15 +22,13 @@
 
 namespace mainera {
 
-Graph::Graph()
-    : m_compiled(false), m_parallel_enabled(false), m_first_node(nullptr) {
+Graph::Graph() : m_compiled(false), m_parallel_enabled(false) {
   // Automatically create an input node as the starting point
   m_input_node = std::make_shared<InputNode>();
   auto input_as_node = std::static_pointer_cast<Node>(m_input_node);
   input_as_node->setSourceNode(nullptr);
   m_nodes.push_back(input_as_node);
   m_node_map[m_input_node->name] = input_as_node;
-  m_first_node = input_as_node;
 }
 
 Graph::~Graph() { clear(); }
@@ -101,11 +99,6 @@ void Graph::addNode(Node::Ptr node) {
   }
 
   m_compiled = false;
-
-  // Track first node with priority: INPUT > PREPROCESS > MODEL
-  if (!m_first_node || shouldReplaceFirstNode(node)) {
-    m_first_node = node;
-  }
 }
 
 void Graph::split(const std::string &branch_name,
@@ -310,7 +303,6 @@ void Graph::clear() {
   m_branch_tails.clear();
   m_compiled = false;
   m_is_branched = false;
-  m_first_node = nullptr;
   m_input_node = nullptr;
   m_node_counter = 0;
 
@@ -319,7 +311,6 @@ void Graph::clear() {
   auto input_as_node = std::static_pointer_cast<Node>(m_input_node);
   m_nodes.push_back(input_as_node);
   m_node_map[m_input_node->name] = input_as_node;
-  m_first_node = input_as_node;
 }
 
 void Graph::enableParallelExecution(bool enable) {
@@ -480,28 +471,6 @@ std::vector<Node::Ptr> Graph::findLeafNodes() const {
   }
 
   return leaves;
-}
-
-bool Graph::shouldReplaceFirstNode(Node::Ptr node) const {
-  if (!m_first_node)
-    return true;
-
-  // Priority: INPUT > PREPROCESS > MODEL > others
-  if (node->type == NodeType::INPUT)
-    return true;
-  if (m_first_node->type == NodeType::INPUT)
-    return false;
-
-  if (node->type == NodeType::PREPROCESS)
-    return true;
-  if (m_first_node->type == NodeType::PREPROCESS)
-    return false;
-
-  if (node->type == NodeType::MODEL &&
-      m_first_node->type != NodeType::PREPROCESS)
-    return true;
-
-  return false;
 }
 
 Node::Ptr Graph::findNodeByName(const std::string &name) const {
