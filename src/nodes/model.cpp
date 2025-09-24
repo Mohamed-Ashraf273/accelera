e86e83805e01305873b06e1c7e245a8efb08639d
khatora <mohamed.khater03@eng-st.cu.edu.eg>
2025-09-24 18:47:40 +0300

    implement model caching and loading mechanism in ModelNode

--- src/nodes/model.cpp
+++ src/nodes/model.cpp
@@ -2,6 +2,13 @@
 #include "nodes/input.hpp"
 #include <pybind11/functional.h>
 #include <pybind11/stl.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/embed.h>
+#include <filesystem>
+#include <chrono>
+#include <iostream>
+using namespace std;
 
 namespace py = pybind11;
 
@@ -41,28 +48,52 @@ void ModelNode::execute() {
 
     py::object model_instance =
         py::module::import("copy").attr("deepcopy")(py_func);
-
+    
+    namespace fs = std::filesystem;
+    py::module_ joblib = py::module_::import("joblib");
+    py::module_ np = py::module_::import("numpy");
+    
+    py::object hash_obj = joblib.attr("hash")(py::make_tuple(model_instance, X, y));
+    std::string hash_value = hash_obj.cast<std::string>();
+    std::string currentPath = fs::current_path().string();
+    fs::create_directory(currentPath+"\\cache");
+    std::string model_path = currentPath + "\\cache\\" + hash_value + ".pkl";
+    
     if (should_create_new_data) {
       py::object X_copy = X.attr("copy")();
       py::object y_copy = y.attr("copy")();
-      try {
-        model_instance.attr("fit")(X_copy, y_copy);
-      } catch (const py::error_already_set &e) {
-        throw std::runtime_error("Python error in model fitting: " +
-                                 std::string(e.what()));
-      }
       auto new_input = std::make_shared<InputNode>();
+      if(fs::exists(model_path)){
+        py::object fitted = joblib.attr("load")(model_path);
+        new_input->setFittedModel(fitted);
+      }else{
+        try {
+          model_instance.attr("fit")(X_copy, y_copy);
+        } catch (const py::error_already_set &e) {
+          throw std::runtime_error("Python error in model fitting: " +
+                                   std::string(e.what()));
+        }
+        new_input->setFittedModel(model_instance);
+        cout<< "saving model to " << model_path << '\n';
+        joblib.attr("dump")(model_instance, model_path);
+      }
       new_input->setInputData(X_copy, y_copy);
-      new_input->setFittedModel(model_instance);
       setOutput(new_input);
     } else {
-      try {
-        model_instance.attr("fit")(X, y);
-      } catch (const py::error_already_set &e) {
-        throw std::runtime_error("Python error in model fitting: " +
-                                 std::string(e.what()));
+      if(fs::exists(model_path)){
+        py::object fitted = joblib.attr("load")(model_path);
+        input->setFittedModel(fitted);
+      }else{
+        try {
+          model_instance.attr("fit")(X, y);
+        } catch (const py::error_already_set &e) {
+          throw std::runtime_error("Python error in model fitting: " +
+                                   std::string(e.what()));
+        }
+        input->setFittedModel(model_instance);
+        cout<< "saving model to " << model_path;
+        joblib.attr("dump")(model_instance, model_path);
       }
-      input->setFittedModel(model_instance);
       setOutput(input);
     }
 
