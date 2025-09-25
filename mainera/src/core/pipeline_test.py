3e30da9faed97a53b2c303e1ffbb1701a2e4bb6f
khatora <mohamed.khater03@eng-st.cu.edu.eg>
2025-09-25 21:17:25 +0300

    feat: refactor CustomModel integration in pipeline tests and remove redundant initialization in CustomClassifier

--- mainera/src/core/pipeline_test.py
+++ mainera/src/core/pipeline_test.py
@@ -238,49 +238,49 @@ class TestPipelineCorrectness:
         manual_accuracy = np.mean(self.y_test == manual_result)
         assert accuracy == manual_accuracy
 
+    class CustomModel(CustomClassifier):
+        def __init__(self, random_state=42):
+            self.random_state = random_state
+            self.centroids_ = None
+            self.classes_ = None
+
+        def fit(self, X, y):
+            np.random.seed(self.random_state)
+            self.classes_ = np.unique(y)
+
+            self.centroids_ = []
+            for class_label in self.classes_:
+                class_points = X[y == class_label]
+                centroid = np.mean(class_points, axis=0)
+                self.centroids_.append(centroid)
+
+            self.centroids_ = np.array(self.centroids_)
+            return self
+
+        def predict(self, X):
+            predictions = []
+            for sample in X:
+                distances = []
+                for centroid in self.centroids_:
+                    distance = np.sqrt(np.sum((sample - centroid) ** 2))
+                    distances.append(distance)
+
+                predicted_class = self.classes_[np.argmin(distances)]
+                predictions.append(predicted_class)
+
+            return np.array(predictions)
+        
     def test_custom_model_integration(self):
-        class CustomModel(CustomClassifier):
-            def __init__(self, random_state=42):
-                super().__init__()
-                self.random_state = random_state
-                self.centroids_ = None
-                self.classes_ = None
-
-            def fit(self, X, y):
-                np.random.seed(self.random_state)
-                self.classes_ = np.unique(y)
-
-                self.centroids_ = []
-                for class_label in self.classes_:
-                    class_points = X[y == class_label]
-                    centroid = np.mean(class_points, axis=0)
-                    self.centroids_.append(centroid)
-
-                self.centroids_ = np.array(self.centroids_)
-                return self
-
-            def predict(self, X):
-                predictions = []
-                for sample in X:
-                    distances = []
-                    for centroid in self.centroids_:
-                        distance = np.sqrt(np.sum((sample - centroid) ** 2))
-                        distances.append(distance)
-
-                    predicted_class = self.classes_[np.argmin(distances)]
-                    predictions.append(predicted_class)
-
-                return np.array(predictions)
 
         p = Pipeline()
         p.preprocess("scale", lambda x: x / 10.0)
-        p.model("custom_rf", CustomModel(random_state=42))
+        p.model("custom_rf", self.CustomModel(random_state=42))
         p.predict("pred", self.test_data)
         pipeline_result = p(self.X, self.y)
 
         X_scaled = self.X / 10.0
         test_scaled = self.test_data / 10.0
-        manual_model = CustomModel(random_state=42)
+        manual_model = self.CustomModel(random_state=42)
         manual_model.fit(X_scaled, self.y)
         manual_result = manual_model.predict(test_scaled)
 
--- mainera/src/custom/classifier.py
+++ mainera/src/custom/classifier.py
@@ -3,12 +3,6 @@ from sklearn.base import ClassifierMixin
 
 
 class CustomClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, random_state=42):
-        super().__init__()
-        self.random_state = random_state
-        self.centroids_ = None
-        self.classes_ = None
-        self.n_features_in_ = None
     def fit(self, X, y):
         raise NotImplementedError(
             "CustomClassifier.fit is not implemented yet."
