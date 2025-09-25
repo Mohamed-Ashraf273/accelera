2670f20badc1becbb9260d5dec66e753e3be4af6
khatora <mohamed.khater03@eng-st.cu.edu.eg>
2025-09-25 20:14:11 +0300

    refactor ModelNode::execute to improve model loading and saving logic

--- src/nodes/model.cpp
+++ src/nodes/model.cpp
@@ -10,7 +10,6 @@
 #include <filesystem>
 #include <chrono>
 #include <iostream>
-using namespace std;
 
 namespace py = pybind11;
 
@@ -45,15 +44,6 @@ void ModelNode::execute() {
 
     py::object X = data->getX();
     py::object y = data->getY();
-    namespace fs = std::filesystem;
-    py::module_ joblib = py::module_::import("joblib");
-    py::module_ np = py::module_::import("numpy");
-    
-    py::object hash_obj = joblib.attr("hash")(py::make_tuple(model_instance, X, y));
-    std::string hash_value = hash_obj.cast<std::string>();
-    std::string currentPath = fs::current_path().string();
-    fs::create_directory(currentPath+"\\cache");
-    std::string model_path = currentPath + "\\cache\\" + hash_value + ".pkl";
 
     if (X.is_none() || y.is_none()) {
       throw std::runtime_error("Model node '" + name +
@@ -67,28 +57,40 @@ void ModelNode::execute() {
 
     py::object model_instance =
         py::module::import("copy").attr("deepcopy")(py_func);
-      if(fs::exists(model_path)){
-        py::object fitted = joblib.attr("load")(model_path);
-        setData(fitted);
-      }
-      else{
+
+
+    py::module_ joblib = py::module_::import("joblib");
+    py::module_ np = py::module_::import("numpy");
+    namespace fs = std::filesystem;
+    py::object hash_obj = joblib.attr("hash")(py::make_tuple(model_instance, X, y));
+    std::string hash_value = hash_obj.cast<std::string>();
+    std::string currentPath = fs::current_path().string();
+    fs::create_directory(currentPath+"\\cache");
+    std::string model_path = currentPath + "\\cache\\" + hash_value + ".pkl";
+
+    if(fs::exists(model_path)){
+      cout<<"load model from "<<model_path << '\n';
+      py::object fitted = joblib.attr("load")(model_path);
+      setData(fitted);
+    }
+    else{
         try {
           model_instance.attr("fit")(X, y);
         } catch (const py::error_already_set &e) {
           throw std::runtime_error("Python error in model fitting: " +
-                                   std::string(e.what()));
-
-          setData(model_instance);
-          joblib.attr("dump")(model_instance, model_path);
+                                  std::string(e.what()));
+        }
+        setData(model_instance);
+        cout<<"save model to "<<model_path << '\n';
+        joblib.attr("dump")(model_instance, model_path);
       }
+    } catch (const py::error_already_set &e) {
+      throw std::runtime_error("ModelNode: Python error during execution: " +
+                              std::string(e.what()));
+    } catch (const std::exception &e) {
+      throw std::runtime_error("ModelNode: Error during execution: " +
+                              std::string(e.what()));
     }
-  } catch (const py::error_already_set &e) {
-    throw std::runtime_error("ModelNode: Python error during execution: " +
-                             std::string(e.what()));
-  } catch (const std::exception &e) {
-    throw std::runtime_error("ModelNode: Error during execution: " +
-                             std::string(e.what()));
-  }
 }
 
 } // namespace mainera
