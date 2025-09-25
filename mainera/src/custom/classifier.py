10d2ed360b334e266cde78d85c1fb5ddcf36a304
khatora <mohamed.khater03@eng-st.cu.edu.eg>
2025-09-25 21:01:51 +0300

    feat: initialize centroids and classes in CustomClassifier

--- mainera/src/custom/classifier.py
+++ mainera/src/custom/classifier.py
@@ -3,6 +3,11 @@ from sklearn.base import ClassifierMixin
 
 
 class CustomClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, random_state=42):
+        super().__init__()
+        self.random_state = random_state
+        self.centroids_ = None
+        self.classes_ = None
     def fit(self, X, y):
         raise NotImplementedError(
             "CustomClassifier.fit is not implemented yet."
