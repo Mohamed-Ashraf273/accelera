88145ade7dd4723a4f87a7bb5b911563c3315c9b
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-09-25 20:50:49 +0300

    add executed graph wrapper

--- mainera/src/wrappers/executed_graph.py
+++ mainera/src/wrappers/executed_graph.py
@@ -3,7 +3,9 @@ class ExecutedGraphWrapper:
         self.__executed_graph = executed_graph
         self.__executed_graph.enableParallelExecution(True)
 
-    def __call__(self, X, y):
+    def __call__(self, X, y, metrics=False, y_true=None):
+        assert not metrics or y_true is not None, (
+            "If metrics is true then you should provide y_true"
+        )
         results = self.__executed_graph.execute(X, y, best_path=False)
-        print(len(results))
         return results
