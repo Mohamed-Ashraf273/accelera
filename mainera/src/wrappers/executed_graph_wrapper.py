d486cab1a8f4724d65040d5bd393ab40f8eeed52
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-09-26 14:40:56 +0300

    remove unnecessary checks

--- mainera/src/wrappers/executed_graph_wrapper.py
+++ mainera/src/wrappers/executed_graph_wrapper.py
@@ -3,15 +3,15 @@ class ExecutedGraphWrapper:
         self.__executed_graph = executed_graph
         self.__executed_graph.enableParallelExecution(True)
 
-    def __call__(self, X, metrics=False, y_true=None):
-        if metrics:
-            if y_true is None:
-                raise ValueError("y_true must be provided when metrics=True")
-
+    def __call__(self, X, y_true=None):
+        if y_true is not None:
             self.__executed_graph.enableDisableMetrics(
                 y_true=y_true, enable=True
             )
+
         results = self.__executed_graph.execute(X, best_path=False)
-        if metrics:
+
+        if y_true is not None:
             self.__executed_graph.enableDisableMetrics(enable=False)
+
         return results
