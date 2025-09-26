fb2a297dbb73d1ca9096274ddd10f88657bfbb4a
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-09-26 23:08:40 +0300

    add tests

--- mainera/src/utils/mainera_utils.py
+++ mainera/src/utils/mainera_utils.py
@@ -1,5 +1,8 @@
+import inspect
 import logging
 import sys
+
+import sklearn.metrics as metrics
 
 interactive = True
 
@@ -22,3 +25,23 @@ def print_msg(message, line_break=True, level="info"):
             logging.error(message)
         else:
             logging.debug(message)
+
+
+def get_metric_object(metric_name):
+    metric_func = getattr(metrics, metric_name, None)
+    return metric_func
+
+
+def metric_validation(metric_func, metric_name):
+    signature = inspect.signature(metric_func)
+    parameters = list(signature.parameters.keys())
+    if ("y_true" not in parameters) and (
+        ("y_pred" not in parameters)
+        or ("y_score" not in parameters)
+        or ("y_prob" not in parameters)
+    ):
+        raise ValueError(
+            f"Metric '{metric_name}' "
+            "does not take (y_true, y_pred) or (y_true, y_score) "
+            "or (y_true, y_prob) as arguments."
+        )
