cbbecb515173e9a4dad5af4938fae71358fc0722
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-10-21 02:46:36 +0300

    update llms

--- mainera/src/models/gemini.py
+++ mainera/src/models/gemini.py
@@ -7,5 +7,5 @@ class Gemini(Llm):
     def __init__(self, model_name: str, api_key: str, temperature=None):
         super().__init__(model_name, api_key=api_key, temperature=temperature)
         self.model = ChatGoogleGenerativeAI(
-            model=self.model_name, api_key=api_key, temperature=temperature
+            model=self.model_name, api_key=api_key, temperature=self.temperature
         )
--- mainera/src/models/ollama.py
+++ mainera/src/models/ollama.py
@@ -6,4 +6,6 @@ from mainera.src.models.llm import Llm
 class Ollama(Llm):
     def __init__(self, model_name: str, api_key: str = None, temprature=None):
         super().__init__(model_name, api_key, temprature)
-        self.model = OllamaModel(model=model_name, temperature=temprature)
+        self.model = OllamaModel(
+            model=self.model_name, temperature=self.temprature
+        )
