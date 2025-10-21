ddfd42a7994b3cf5dd8d821b957ca35d88782405
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-10-21 18:42:00 +0300

    add endpoint inferences

--- mainera/src/models/groq.py
+++ mainera/src/models/groq.py
@@ -1,11 +1,13 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 
 from mainera.src.models.llm import Llm
 
 
-class Gemini(Llm):
+class GroqModel(Llm):
     def __init__(self, model_name: str, api_key: str, temperature: float = 0.1):
         super().__init__(model_name, api_key=api_key, temperature=temperature)
-        self.model = ChatGoogleGenerativeAI(
-            model=self.model_name, api_key=api_key, temperature=self.temperature
+        self.model = ChatGroq(
+            model=self.model_name,
+            api_key=self.api_key,
+            temperature=self.temperature,
         )
--- mainera/src/models/ollama.py
+++ mainera/src/models/ollama.py
@@ -4,8 +4,10 @@ from mainera.src.models.llm import Llm
 
 
 class Ollama(Llm):
-    def __init__(self, model_name: str, api_key: str = None, temprature=None):
-        super().__init__(model_name, api_key, temprature)
+    def __init__(
+        self, model_name: str, api_key: str = None, temperature: float = 0.1
+    ):
+        super().__init__(model_name, api_key, temperature)
         self.model = OllamaModel(
-            model=self.model_name, temperature=self.temprature
+            model=self.model_name, temperature=self.temperature
         )
