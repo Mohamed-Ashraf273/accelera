ee2c909388a7657f6affdd509df0b3c3d0a2dbb2
Nesma Osama <128908402+Nesma-Osama@users.noreply.github.com>
2025-12-19 14:31:46 +0200

    solve leakage problems (#103)
    * solve leakage problems
    
    * format
    
    ---------
    
    Co-authored-by: Mohamed-Ashraf273 <ma2736666@gmail.com>

--- accelera/src/automl/utils/preprocessing.py
+++ accelera/src/automl/utils/preprocessing.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
@@ -6,13 +5,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
 
 # ----------------------------------------------------------
 # test on taitanic dataset
-df_titanic = pd.read_csv("Testing_dataset/Titanic-Dataset.csv")
+df_titanic = pd.read_csv("../../../../examples/Titanic-Dataset.csv")
 # test one  house price data set
-# Download latest version
-df_price = pd.read_csv("Testing_dataset/Housing.csv")
+df_price = pd.read_csv("../../../../examples/Housing.csv")
+# test on multi-class classification dataset
+df_multi_class = pd.read_csv("../../../../examples/train.csv")
 
 
 # ----------------------------------------------------------
@@ -39,9 +40,9 @@ def is_drop_column(df, info, col):
 
 
 # get the outliers info
-def outliers_info(df, info, col):
-    Q1 = df[col].quantile(0.25)
-    Q3 = df[col].quantile(0.75)
+def outliers_info(info, col):
+    Q1 = info[col]["Q1"]
+    Q3 = info[col]["Q3"]
     IQR = Q3 - Q1
     lower = Q1 - 1.5 * IQR
     upper = Q3 + 1.5 * IQR
@@ -50,9 +51,18 @@ def outliers_info(df, info, col):
 
 # cap the outliers
 def cap_outliers(df, info, col):
-    if "outliers_info" in info[col]:
+    if (
+        "outliers_info" in info[col]
+        and info[col]["type"] in ["float64", "int64"]
+        and not info[col].get("binary", False)
+    ):
         lower, upper = info[col]["outliers_info"]
         df[col] = df[col].clip(lower, upper)
+
+
+def check_binary(col, info):
+    if info[col].get("n_unique", 0) == 2 or info[col].get("type") == "bool":
+        return True
 
 
 # get each column info
@@ -66,19 +76,31 @@ def get_data_info(X_train, y_train, target_col):
             "type": df_new[col].dtype,
             "n_unique": df_new[col].nunique(),
             "p_unique": df_new[col].nunique() / df_new.shape[0],
+            "missing": df_new[col].isna().sum(),
             "p_missing": df_new[col].isna().sum() / df_new.shape[0],
             "is_constant": df_new[col].nunique() == 1,
         }
+        print(f"Processing column: {col},{info[col]['n_unique']} unique values")
         if is_drop_column(df_new, info, col) and col != target_col:
             col_drop.append(col)
             info.pop(col)
             continue
+        info[col]["binary"] = check_binary(col, info)
         if info[col]["type"] in ["int64", "float64"]:
             info[col]["skew"] = df_new[col].skew()
             info[col]["variance"] = df_new[col].var()
-            info[col]["outliers_info"] = outliers_info(df_new, info, col)
+            info[col]["Q1"] = df_new[col].quantile(0.25)
+            info[col]["Q3"] = df_new[col].quantile(0.75)
+            info[col]["outliers_info"] = outliers_info(info, col)
             info[col]["min"] = df_new[col].min()
             info[col]["max"] = df_new[col].max()
+            info[col]["median"] = df_new[col].median()
+            info[col]["mean"] = df_new[col].mean()
+        if info[col]["type"] == "object" or info[col]["binary"]:
+            info[col]["distribution"] = (
+                df_new[col].value_counts(normalize=True).to_dict()
+            )
+            info[col]["mode"] = df_new[col].mode()[0]
 
     return info, col_drop
 
@@ -91,34 +113,21 @@ def drop_duplicates(df):
 def drop_columns(X_train, X_test, col_drop):
     # Drop columns
     X_train.drop(columns=col_drop, inplace=True)
-    X_test.drop(columns=col_drop, inplace=True)
+    X_test.drop(columns=col_drop, inplace=True, errors="ignore")
 
 
 # handle missing values imputation
 def handle_missing(df, info, col):
     if info[col].get("p_missing", 0) != 0:
         col_type = col_type = info[col].get("type")
-        if col_type == "object":
-            mode = df[col].mode()[0]
+        if col_type in ["object"] or info[col].get("binary", False):
+            mode = info[col]["mode"]
             print(f"{col} Fill missing mode value")
             df[col] = df[col].fillna(mode)
         else:
-            median = df[col].median()
+            median = info[col]["median"]
             print(f"{col} Fill missing median value")
             df[col] = df[col].fillna(median)
-
-
-def handle_skew(df, info, col, shift_value=0):
-    if "skew" in info[col]:
-        skew = info[col]["skew"]
-        if abs(skew) > 0.75:
-            shift_value = 0 if df[col].min() >= 0 else abs(df[col].min()) + 1
-            print(f"{col} Apply log1p to reduce skewness")
-            df[col] = df[col].apply(
-                lambda x: np.log1p(x + shift_value) if x >= 0 else x
-            )
-            return True
-    return False
 
 
 # handle encoding categorical features
@@ -144,9 +153,9 @@ def type_encoding(
             frequency_encoding_cols.append(col)
 
 
-def frequency_encoding(df, col):
-    mapping = df[col].value_counts(normalize=True)
+def frequency_encoding(df, col, mapping):
     df[col] = df[col].map(mapping)
+    df[col] = df[col].fillna(0)
     print(f"{col} frequency encoding")
 
 
@@ -159,25 +168,33 @@ def handle_label_encoding_features(X_train, X_test, colums):
     return X_train, X_test
 
 
-def handle_one_hot_encoding_features(X_train, X_test, colums):
-    X_train = pd.get_dummies(X_train, columns=colums, drop_first=True)
-    X_test = pd.get_dummies(X_test, columns=colums, drop_first=True)
-    X_train, X_test = X_train.align(X_test, join="left", axis=1, fill_value=0)
-    print(f"{colums} one hot encoder")
+def handle_one_hot_encoding_features(X_train, X_test, columns):
+    X_train_dummies = pd.get_dummies(
+        X_train[columns], drop_first=True, dtype=int
+    )
+    X_test_dummies = pd.get_dummies(X_test[columns], drop_first=True, dtype=int)
+    X_test_dummies = X_test_dummies.reindex(
+        columns=X_train_dummies.columns, fill_value=0
+    )
+    X_train = X_train.drop(columns=columns).join(X_train_dummies)
+    X_test = X_test.drop(columns=columns).join(X_test_dummies)
+    print(f"One-hot encoded {columns}")
     return X_train, X_test
 
 
-def handle_frequency_encoding_features(X_train, X_test, colums):
+def handle_frequency_encoding_features(X_train, X_test, info, colums):
     for col in colums:
-        frequency_encoding(X_train, col)
-        frequency_encoding(X_test, col)
-        X_test = X_test.fillna(0)
+        mapping = info[col]["distribution"]
+        frequency_encoding(X_train, col, mapping)
+        frequency_encoding(X_test, col, mapping)
+
     return X_train, X_test
 
 
 def handle_encoding_features(
     X_train,
     X_test,
+    info,
     label_encoding_cols,
     one_hot_encoding_cols,
     frequency_encoding_cols,
@@ -189,7 +206,7 @@ def handle_encoding_features(
         X_train, X_test, one_hot_encoding_cols
     )
     X_train, X_test = handle_frequency_encoding_features(
-        X_train, X_test, frequency_encoding_cols
+        X_train, X_test, info, frequency_encoding_cols
     )
     return X_train, X_test
 
@@ -197,7 +214,6 @@ def handle_encoding_features(
 # features preprocessing
 # - cap outliers
 # - handle missing
-# - handle skewness
 # - handle encoding categorical features
 def features_preprocessing(X_train, X_test, info):
     label_encoding_cols = []
@@ -210,11 +226,6 @@ def features_preprocessing(X_train, X_test, info):
         # Outliers
         cap_outliers(X_train, info, col)
         cap_outliers(X_test, info, col)
-        # Skewness
-        col_min = info[col].get("min", 0)
-        shift_value = 0 if col_min >= 0 else abs(col_min) + 1
-        handle_skew(X_train, info, col, shift_value)
-        handle_skew(X_test, info, col, shift_value)
         # Encoding
 
         type_encoding(
@@ -227,6 +238,7 @@ def features_preprocessing(X_train, X_test, info):
     X_train, X_test = handle_encoding_features(
         X_train,
         X_test,
+        info,
         label_encoding_cols,
         one_hot_encoding_cols,
         frequency_encoding_cols,
@@ -248,8 +260,6 @@ def target_preprocessing(y_train, y_test, info, col):
     else:
         y_train_df = y_train.to_frame()
         y_test_df = y_test.to_frame()
-        # handle_skew(y_train_df, info, col, shift_value)
-        # handle_skew(y_test_df, info, col, shift_value)
         y_train = y_train_df[col]
         y_test = y_test_df[col]
     return y_train, y_test
@@ -269,6 +279,7 @@ def split_data(df, target_col):
 def common_preprocessing(df, target_col):
     drop_duplicates(df)
     X_train, X_test, y_train, y_test = split_data(df, target_col)
+
     info, col_drop = get_data_info(X_train, y_train, target_col)
     drop_columns(X_train, X_test, col_drop)
     X_train, X_test = features_preprocessing(X_train, X_test, info)
@@ -287,18 +298,14 @@ model.fit(X_train, y_train)
 print("Tatianic Score", model.score(X_test, y_test))
 # --------------------------------------------------------------
 X_train, y_train, X_test, y_test = common_preprocessing(df_price, "price")
-#
+
 print("House price data")
 print(X_train)
-# y_train = np.log1p(y_train)
-# y_test = np.log1p(y_test)
-
 
 model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 model.score(X_test, y_test)
-#
 
 
 model = LinearRegression()
@@ -306,4 +313,17 @@ model.fit(X_train, y_train)
 
 print("House price Score", model.score(X_test, y_test))
 print("MAE:", mean_absolute_error(y_test, model.predict(X_test)))
-#
+print("--------------------------------------------------")
+print(df_multi_class.head())
+X_train, y_train, X_test, y_test = common_preprocessing(
+    df_multi_class, "Segmentation"
+)
+print("Multi-class data")
+print(X_train)
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+print("Multi-class Score", model.score(X_test, y_test))
+xgb_model = XGBClassifier(eval_metric="mlogloss", random_state=42)
+
+xgb_model.fit(X_train, y_train)
+print("Multi-class XGB Score", xgb_model.score(X_test, y_test))
