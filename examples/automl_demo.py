59cb8151ac551ecd5d3d0df284a8421c329bacc8
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-12-20 18:40:59 +0200

    change name

--- examples/automl_demo.py
+++ examples/automl_demo.py
@@ -2,9 +2,9 @@ import os
 
 import pandas as pd
 
-from accelera.src.automl.core.agent import AutoMLAgent
+from accelera.src.automl.core.agent import AutoAccelera
 
-agent = AutoMLAgent()
+agent = AutoAccelera()
 current_dir = os.path.dirname(__file__)
 df = pd.read_csv(os.path.join(current_dir, "Titanic-Dataset.csv"))
 best_pipeline = agent.get_pipeline(df, "Survived")
