39acf2ff24119ede587f08d3139380711e6be7dc
Nesma-Osama <Nesma.Ahmed03@eng-st.cu.edu.eg>
2026-02-08 18:36:32 +0200

    change some tests

--- accelera/src/automl/wrappers/custom_transform_test.py
+++ accelera/src/automl/wrappers/custom_transform_test.py
@@ -11,7 +11,7 @@ from sklearn.compose import ColumnTransformer
 
 class TestCustomTransform:
     def test_frequency_encoder_transform(self):
-        df = pd.DataFrame(
+        data = pd.DataFrame(
             {
                 "color": ["red", "blue", "red", "green", "blue", "blue"],
                 "size": ["S", "L", "L", "S", "S", "L"],
@@ -31,8 +31,8 @@ class TestCustomTransform:
             }
         )
         transformer = FrequencyEncoderTransform()
-        transformer.fit(df)
-        transformed = transformer.transform(df)
+        transformer.fit(data)
+        transformed_data_data = transformer.transform(data)
         assert "color" in transformer.mapping_
         assert "size" in transformer.mapping_
         assert transformer.mapping_["color"] == {
@@ -41,23 +41,23 @@ class TestCustomTransform:
             "green": 0.16666666666666666,
         }
         assert transformer.mapping_["size"] == {"S": 0.5, "L": 0.5}
-        np.testing.assert_almost_equal(transformed, expected_output.values)
+        assert np.array_equal(transformed_data_data, expected_output.values)
 
-    def test_frequency_encoder_transform_unseen(self):
-        df_train = pd.DataFrame(
+    def test_frequency_encoder_transform_unseen_category(self):
+        training_data = pd.DataFrame(
             {
                 "color": ["red", "blue", "red", "green", "blue", "blue"],
                 "size": ["S", "L", "L", "S", "S", "L"],
             }
         )
-        df_test = pd.DataFrame({"color": ["red", "yellow"], "size": ["S", "XL"]})
+        testing_data = pd.DataFrame({"color": ["red", "yellow"], "size": ["S", "XL"]})
         expected_output = pd.DataFrame(
             {"color": [0.3333333333333333, 0], "size": [0.5, 0]}
         )
         transformer = FrequencyEncoderTransform()
-        transformer.fit(df_train)
-        transformed = transformer.transform(df_test)
-        np.testing.assert_almost_equal(transformed, expected_output.values)
+        transformer.fit(training_data)
+        transformed_data = transformer.transform(testing_data)
+        assert np.array_equal(transformed_data, expected_output.values)
 
     def test_frequency_encoder_transform_pipeline(self):
         df = pd.DataFrame(
@@ -80,19 +80,19 @@ class TestCustomTransform:
             }
         )
         pipeline = Pipeline([("freq_encoder", FrequencyEncoderTransform())])
-        column_transformer = ColumnTransformer(
+        transformer = ColumnTransformer(
             [("freq_encoder", pipeline, ["color", "size"])]
         )
-        transformed = column_transformer.fit_transform(df)
-        np.testing.assert_almost_equal(transformed, expected_output.values)
+        transformed_data = transformer.fit_transform(df)
+        assert np.array_equal(transformed_data, expected_output.values)
 
     def test_flatten_1d_transform(self):
         data = np.array([["Hello"], ["World"]])
         expected_output = np.array(["Hello", "World"])
         transformer = Flatten1DTransform(func=lambda x: x.ravel())
-        transformed = transformer.fit_transform(data)
-        assert transformed.shape == expected_output.shape
-        np.testing.assert_array_equal(transformed, expected_output)
+        transformed_data = transformer.fit_transform(data)
+        assert transformed_data.shape == expected_output.shape
+        assert np.array_equal(transformed_data, expected_output)
 
     def test_flatten_1d_transform_pipeline(self):
         data = pd.DataFrame({"col": ["Hello", "World"]})
@@ -106,10 +106,10 @@ class TestCustomTransform:
                 )
             ]
         )
-        column_transformer = ColumnTransformer([("flatten", pipeline, ["col"])])
-        transformed = column_transformer.fit_transform(data)
-        assert transformed.shape == expected_output.shape
-        np.testing.assert_array_equal(transformed, expected_output)
+        transformer = ColumnTransformer([("flatten", pipeline, ["col"])])
+        transformed_data = transformer.fit_transform(data)
+        assert transformed_data.shape == expected_output.shape
+        assert np.array_equal(transformed_data, expected_output)
 
     def test_iqr_transform(self):
         data = pd.DataFrame(
@@ -124,7 +124,7 @@ class TestCustomTransform:
         )
         transformer = IQRTransform(info=info, cols=["col", "col2"])
         output = transformer.fit_transform(data.values)
-        np.testing.assert_array_equal(output, expected_output.values)
+        assert np.array_equal(output, expected_output.values)
 
     def test_iqr_transform_pipeline_data_frame(self):
         data = pd.DataFrame(
@@ -140,11 +140,11 @@ class TestCustomTransform:
         pipeline = Pipeline(
             [("iqr_transform", IQRTransform(info=info, cols=["col", "col2"]))]
         )
-        column_transformer = ColumnTransformer(
+        transformer = ColumnTransformer(
             [("iqr_transform", pipeline, ["col", "col2"])]
         )
-        output = column_transformer.fit_transform(data)
-        np.testing.assert_array_equal(output, expected_output.values)
+        output = transformer.fit_transform(data)
+        assert np.array_equal(output, expected_output.values)
 
     def test_iqr_transform_pipeline_numpy(self):
         data = pd.DataFrame(
@@ -160,6 +160,6 @@ class TestCustomTransform:
         pipeline = Pipeline(
             [("iqr_transform", IQRTransform(info=info, cols=["col", "col2"]))]
         )
-        column_transformer = ColumnTransformer([("iqr_transform", pipeline, [0, 1])])
-        output = column_transformer.fit_transform(data.values)
-        np.testing.assert_array_equal(output, expected_output.values)
+        transformer = ColumnTransformer([("iqr_transform", pipeline, [0, 1])])
+        output = transformer.fit_transform(data.values)
+        assert np.array_equal(output, expected_output.values)
