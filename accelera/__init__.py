64f2d7fa40844cb76163a676d615307f0952d7d8
Mohamed-Ashraf273 <ma2736666@gmail.com>
2026-03-16 13:53:11 +0200

    modify parallelizer

--- accelera/__init__.py
+++ accelera/__init__.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 # Add build path BEFORE importing anything else
-print("Setting up accelera module path...")
 current_dir = os.path.dirname(__file__)
 repo_root = current_dir
 
