a0a441a60908bea92935e6dd2bd53d03245e6be0
Mohamed-Ashraf273 <ma2736666@gmail.com>
2026-06-20 00:54:20 +0300

    fix big issue

---
+++ benchmark/frontend/src/components/createBenchmark.jsx
@@ -0,0 +1,249 @@
+import { useState, useEffect } from "react";
+import Navigation from "./navigation";
+import "./createBenchmark.css";
+function CreateBenchmark() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [loading, setLoading] = useState(false);
+  const [metrics, setMetrics] = useState([]);
+  const [metric, setMetric] = useState({});
+  const [problemMessage, setProblemMessage] = useState(null);
+  const [checking, setChecking] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    targetColumn: "",
+    datasetLink: "",
+    testSetWithoutPredictionsLink: "",
+    predictedColumnLink: "",
+    problemType: "classification",
+    evaluationMetric: "",
+    metricParamaters: "",
+    createdBy: user._id,
+  });
+  const fetchMetric = async (problemType) => {
+    setLoading(true);
+    const results = await fetch(
+      `http://localhost:3000/metrics/problem-type/${problemType}`,
+    );
+    const data = await results.json();
+    setMetrics(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchMetric(form.problemType);
+  }, []);
+  const handleChooseMetric = (e) => {
+    e.preventDefault();
+    const selectedMetric = metrics.find(
+      (metric) => metric._id === e.target.value,
+    );
+    setMetric(selectedMetric);
+    handleChange(e);
+  };
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+    if (e.target.name === "problemType") {
+      fetchMetric(e.target.value);
+    }
+  };
+  const handleParamter = (key, value) => {
+    setForm((prev) => ({
+      ...prev,
+      metricParamaters: {
+        ...prev.metricParamaters,
+        [key]: value,
+      },
+    }));
+  };
+  const checkFill = () => {
+    if (!form.title || form.title?.trim === "") {
+      setProblemMessage("Fill Title filed");
+      return false;
+    }
+    if (!form.description || form.description?.trim === "") {
+      setProblemMessage("Fill description filed");
+      return false;
+    }
+    if (!form.targetColumn || form.targetColumn?.trim === "") {
+      setProblemMessage("Fill targetColumn filed");
+      return false;
+    }
+    if (!form.datasetLink || form.datasetLink?.trim === "") {
+      setProblemMessage("Fill datasetLink filed");
+      return false;
+    }
+    if (
+      !form.testSetWithoutPredictionsLink ||
+      form.testSetWithoutPredictionsLink?.trim === ""
+    ) {
+      setProblemMessage("Fill testSetWithoutPredictionsLink filed");
+      return false;
+    }
+    if (!form.predictedColumnLink || form.predictedColumnLink?.trim === "") {
+      setProblemMessage("Fill predictedColumnLink filed");
+      return false;
+    }
+    if (!form.evaluationMetric || form.evaluationMetric?.trim === "") {
+      setProblemMessage("Fill evaluationMetric filed");
+      return false;
+    }
+    setProblemMessage("");
+    return true;
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setChecking(true);
+    if (!checkFill()) return;
+    try {
+      const response = await fetch("http://localhost:3000/benchmark/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        setProblemMessage(data.message);
+        setChecking(false);
+
+        return;
+      }
+      alert("Benchmark created successfully!");
+      setChecking(false);
+      setForm({
+        title: "",
+        description: "",
+        targetColumn: "",
+        datasetLink: "",
+        testSetWithoutPredictionsLink: "",
+        predictedColumnLink: "",
+        problemType: "classification",
+        evaluationMetric: "",
+        metricParamaters: "",
+        createdBy: user._id,
+      });
+    } catch (err) {
+      console.error(err.response?.data || err.message);
+      alert(err.response?.data?.message || "Error creating benchmark");
+    }
+  };
+
+  return (
+    <div className="create-benchmark-page">
+      <Navigation />
+      <div className="create-benchmark-content">
+        <h2>Create Benchmark</h2>
+        {loading && <p className="loading">Loading...</p>}
+        {!loading && (
+          <form onSubmit={handleSubmit} className="create-benchmark-form">
+            <div className="form-input">
+              <p>Title</p>
+              <input
+                value={form.title}
+                name="title"
+                placeholder="e.g. house_price"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-input">
+              <p>Description</p>
+              <textarea
+                value={form.description}
+                rows={5}
+                cols={50}
+                name="description"
+                placeholder="e.g. predict price of regression data"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-input">
+              <p>Target Column Name in the dataset</p>
+              <input
+                value={form.targetColumn}
+                name="targetColumn"
+                placeholder="e.g. price"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-input">
+              <p>Dataset Google Drive Link</p>
+              <input
+                name="datasetLink"
+                onChange={handleChange}
+                value={form.datasetLink}
+              />
+            </div>
+            <div className="form-input">
+              <p>Test Set Google Drive Link</p>
+              <input
+                name="testSetWithoutPredictionsLink"
+                onChange={handleChange}
+                value={form.testSetWithoutPredictionsLink}
+              />
+            </div>
+            <div className="form-input">
+              <p>Predicted Column Google Drive Link</p>
+              <input
+                name="predictedColumnLink"
+                onChange={handleChange}
+                value={form.predictedColumnLink}
+              />
+            </div>
+            <div className="form-input">
+              <p>problem Type</p>
+              <select
+                name="problemType"
+                onChange={handleChange}
+                value={form.problemType}
+              >
+                <option value="classification">Classification</option>
+                <option value="regression">Regression</option>
+              </select>
+            </div>
+            <div className="form-input">
+              <div>
+                <p>{form.problemType} Evaluation Metric</p>
+                <select name="evaluationMetric" onChange={handleChooseMetric}>
+                  {metrics.map((metric, index) => (
+                    <option key={index} value={metric._id}>
+                      {metric.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="form-input">
+                {metric.neededParameters &&
+                  Object.entries(metric.neededParameters).map(
+                    ([key, values]) => (
+                      <div key={key}>
+                        <p>{key}</p>
+                        <select
+                          value={form.metricParamaters?.[key] || values[0]}
+                          onChange={(e) => handleParamter(key, e.target.value)}
+                        >
+                          {values.map((value, index) => (
+                            <option value={value} key={index}>
+                              {value}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    ),
+                  )}
+              </div>
+            </div>
+            {!checking && (
+              <>
+                <p>{problemMessage}</p>
+                <button type="submit">Create Benchmark</button>
+              </>
+            )}
+            {checking && <p>Uploading....</p>}
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
+export default CreateBenchmark;
